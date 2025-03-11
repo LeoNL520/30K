dc63dae4dd58107be590d972fdd6f2c6f7bdf304
Leo520 <abyssgamefi@gmail.com>
2025-03-12 00:12:27 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'repair-form-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 安裝事件 - 預先緩存重要資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// 激活事件 - 清理舊緩存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => {
+          return name !== CACHE_NAME;
+        }).map((name) => {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+// 攔截請求 - 提供離線支持
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request)
+          .then((fetchResponse) => {
+            return fetchResponse;
+          })
+          .catch(() => {
+            // 無網絡連接時提供基本離線頁面
+            if (event.request.mode === 'navigate') {
+              return caches.match('/index.html');
+            }
+            return new Response('離線模式無法訪問此資源', { 
+              status: 503,
+              statusText: 'Service Unavailable',
+              headers: new Headers({
+                'Content-Type': 'text/plain'
+              })
+            });
+          });
+      })
+  );
+});
